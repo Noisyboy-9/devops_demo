b2c87c17d36840b1b656cdda90c32cf087f7f4ac
sina shariati <sina.shariati@yahoo.com>
2023-12-29 20:27:05 +0330

    Finish implementing fib() and simple http server

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+func fib(n int) int {
+	if n == 0 {
+		return 0
+	}
+
+	if n == 1 {
+		return 1
+	}
+
+	return fib(n-1) + fib(n-2)
+}
+
+func calculateFib(c echo.Context) error {
+	inputStr := c.Param("n")
+	n, err := strconv.Atoi(inputStr)
+	if err != nil {
+		panic(err)
+	}
+	return c.String(http.StatusOK, fmt.Sprintf("hello, result of fib(%s) = %v", inputStr, fib(n)))
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/fib/:n", calculateFib)
+	e.Logger.Fatal(e.Start(":8080"))
+}
